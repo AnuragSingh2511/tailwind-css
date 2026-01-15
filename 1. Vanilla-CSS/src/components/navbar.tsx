5b8f33fd96f03c4d323447f2c9906ba53cfdc424
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-01-15 11:10:33 +0530

    Initial commit: Add Vanilla-CSS and Finta Landing Page projects

---
+++ 1. Vanilla-CSS/src/components/navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const Navbar = () => {
+    const links = [
+        {
+          title: 'Guide',
+          href: '#',
+        },
+        {
+          title: 'Pricing',
+          href: '#',
+        },
+        {
+          title: 'Login',
+          href: '#',
+        }
+    ]
+  return ( 
+  <div className="navbar-root">
+    <div className="logo">Fintech</div>
+    <div className="links">
+        {links.map((link, idx) => ( 
+            <a className="link-items" key = {link.title} href={link.href}>{link.title}</a>
+    ) ) }
+    </div>
+    <button className="btn">Start free trial</button>
+  </div>
+  );
+};
+
