e1edd17c19917c19a22b51191c9c07548a82bb13
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-01-17 12:16:32 +0530

    refactor: tailwind-className issues for finta navbar

---
+++ finta-landing-page/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "../components/container";
+import Navbar from "../components/navbar";
+// import { Navbar } from "../components/navbar";
+
+export default function Home() {
+  return (
+    <Container className="bg-red-500">
+      <Navbar />
+    </Container>
+  );
+}
---
+++ finta-landing-page/src/components/container.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { cn } from "../lib/utils";
+
+// export function Container ({
+//   children,
+//   className,
+// }:{
+//   children: React.ReactNode;
+//   className?: string;
+// }) {
+//   console.log(className);
+//   return <div className= {cn("max-w-4xl mx-auto px-4 md:py-8", className)}>{children}</div>;
+// };
+
+// both are not same..major diff bw arrow fn and keyword based fn (interview- javascript)
+
+export const Container = ({
+  children,
+  className,
+}:{
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  console.log(className);
+  return <div className= {cn("max-w-4xl mx-auto px-4 md:py-8", className)}>{children}</div>;
+};
