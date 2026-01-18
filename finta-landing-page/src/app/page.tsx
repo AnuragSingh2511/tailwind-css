aa081833265a7da8d0d8a6999f620b043d8a7a5b
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-01-18 10:32:16 +0530

    feat: progress

--- finta-landing-page/src/app/page.tsx
+++ finta-landing-page/src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Container } from "../components/container";
-import Navbar from "../components/navbar";
-// import { Navbar } from "../components/navbar";
+import { Navbar } from "../components/navbar";
+
 
 export default function Home() {
   return (
-    <Container className="bg-red-500">
-      <Navbar />
-    </Container>
+    <div className="flex flex-col items-center h-screen relative bg-blue-100">
+      {/* <div className="h-40 w-full absolute inset-0 bg-radial from-white to-transparent"/> */}
+      <Container> 
+        <Navbar />
+      </Container>
+    </div>
   );
 }
--- finta-landing-page/src/components/container.tsx
+++ finta-landing-page/src/components/container.tsx
@@ -22,5 +22,7 @@ export const Container = ({
   className?: string;
 }) => {
   console.log(className);
-  return <div className= {cn("max-w-4xl mx-auto px-4 md:py-8", className)}>{children}</div>;
+  return <div className= {cn("max-w-7xl mx-auto px-4 md:py-8", className)}>
+    {children}
+    </div>;
 };
